cd8044e39e9d5604278ec5a7f78fc3c9523765ff
Karan <karansharma192004@gmail.com>
2024-12-25 14:04:17 +0530

    landing page endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
+      <Route path="/" element={<Signup/>}/>
       <Route path="/signup" element={<Signup/>}/>
       <Route path="/signin" element={<Signin/>}/>
       <Route path="/dashboard" element={<Homepage/>}/>
